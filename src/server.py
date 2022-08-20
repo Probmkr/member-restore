49adb658e7364683c189aa481968050e7e79c35c
Probmkr <Thanatos9911@gmail.com>
2022-08-20 13:50:46 +0900

    leave コマンドの実行の順番を変更した

--- src/server.py
+++ src/server.py
@@ -209,11 +209,11 @@ async def backup(interaction: disnake.ApplicationCommandInteraction, srvid: str)
 
 
 @bot.slash_command(name="leave", guild_ids=admin_guild_ids, description="Botをサーバーから退出させます")
-async def slash_leave(interaction: disnake.ApplicationCommandInteraction, guild_id: int = None):
+async def slash_leave(interaction: disnake.ApplicationCommandInteraction, guild_id: str):
     if int(interaction.author.id) in admin_users:
         try:
+            await interaction.response.send_message(f"{guild_id}から退出します")
             await bot.get_guild(int(guild_id)).leave()
-            await interaction.response.send_message(f"{guild_id}から退出しました")
         except AttributeError:
             await interaction.response.send_message(f"{guild_id}から退出できませんでした")
     else:
